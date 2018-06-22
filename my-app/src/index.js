cc8cb28cdad62105b3735bb3d5fbddfe6763f1e8
allen <allen@allendeMacBook-Pro-5.local>
2018-06-22 22:09:56 +0800

    feat: 添加导航栏和hellow哦了rd

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HelloWorld from "./c1/hellowold";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const styles = {
+  fontFamily: "sans-serif",
+  paddingLeft: "250px"
+};
+
+const routeMap = {
+  hello: HelloWorld
+};
+
+class App extends React.PureComponent {
+  handleLinkClick = key => {
+    window.location.hash = `#${key}`;
+    this.forceUpdate();
+  };
+  render() {
+    const currentPage = document.location.hash.replace("#", "");
+
+    const CurrentPage = routeMap[currentPage] || HelloWorld;
+    return (
+      <div style={styles}>
+        <ul className="menu-list">
+          {Object.keys(routeMap).map(key => (
+            <li
+              key={key}
+              className={
+                key === document.location.hash.replace("#", "")
+                  ? "is-active"
+                  : ""
+              }
+              style={{ listStyle: "none" }}
+            >
+              <span className="link" onClick={() => this.handleLinkClick(key)}>
+                {key}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div style={{ padding: "30px 0" }}>
+          <CurrentPage />
+        </div>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
