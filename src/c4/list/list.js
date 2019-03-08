30d9ca8936476a535d573a53e6c8dc793b36c6af
AllenChinese <13402202685@163.com>
2019-03-09 00:05:13 +0800

    deploy-20190309-000513

--- src/c4/list/list.js
+++ src/c4/list/list.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 
+function ItemList(props) {
+  return (
+    <li>{props.value * 2}</li>
+  )
+}
+
 export default class List extends Component {
   constructor(props) {
     super(props)
-    const numbers = props.numbers
+    this.numbers = props.numbers
   }
 
   render() {
     return (
       <ul>
         {this.numbers.map((number) =>
-          <li key={number.toString()}>{number}</li>
+          <ItemList value={number} key={number.toString()} />
         )}
       </ul>
     )
   }
 }
+
